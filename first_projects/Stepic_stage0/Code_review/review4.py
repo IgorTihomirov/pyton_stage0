983bd8d68bdf0ecd2843aa948aeb6adccb11cda9
Igor <i.n.tihomirov@yandex.ru>
2024-04-18 22:20:55 +0300

    new file: review4.py

---
+++ first_projects/Stepic_stage0/Code_review/review4.py
@@ -0,0 +1,12 @@
+n = int(input())
+max_digit = -1 # здесь была ошибка n % 10
+while n > 0:
+    digit = n % 10
+    if digit % 3 == 0:
+        if digit > max_digit: # здесь была ошибка if digit < max_digit: . Последня цифра должна быть больше, чтобы ее перезаписть в max_digit
+            max_digit = digit # здесь была ошибка digit = max_digit. Нам нужно перезаписывать max_digit.
+    n = n // 10 # здесь была ошибка n % 10. При каждом цикле мы должны отмечать последнюю цифру числа, чтобы проверить все цифры
+if max_digit == -1: # здесь была ошибка if max_digit == 0:. Если max_digit останется - 1, значит за все итерации так и не нашлось цифры кратной 3
+    print('NO')
+else:
+    print(max_digit)
